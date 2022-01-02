ca5e2b31917a1644c4b1ea7ecc18d598e7b1e94c
mert özdemir <mertozdemir91@hotmail.com>
2022-01-03 00:08:24 +0300

    remainingcards

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const cardsContainer = document.getElementById("cards")
 const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
 const header = document.getElementById('header')
+const remainingText = document.getElementById('remaining')
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
+            remainingText.textContent = `Remaining Cards: ${data.remaining}`
             deckId = data.deck_id
         })
 }
@@ -18,6 +20,7 @@ drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
+            remainingText.textContent = `Remaining Cards: ${data.remaining}`
             cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
             `
