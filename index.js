2689fc0322072af03a75f3cea8c80f0ab13fd734
mert özdemir <mertozdemir91@hotmail.com>
2022-01-02 23:48:16 +0300

    winnercard

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let deckId
 const cardsContainer = document.getElementById("cards")
 const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
+const header = document.getElementById('header')
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
@@ -23,5 +24,21 @@ drawCardBtn.addEventListener("click", () => {
             cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
+            header.textContent = determineWinner(data.cards[0], data.cards[1])
         })
 })
+
+function determineWinner(card1,card2){
+    const valueOptions = ["2","3","4","5","6","7","8","9","10","JACK","QUEEN","KING","ACE"]
+
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
+
+    if (card1ValueIndex > card2ValueIndex) {
+        return "Card 1 wins!"
+    } else if (card1ValueIndex < card2ValueIndex) {
+        return "Card 2 wins!"
+    } else {
+        return "Draw!"
+    }
+}
